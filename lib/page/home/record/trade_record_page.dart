a85167401dddd080c08f4b4a067c71b37bd55e3e
CPJJ <aichonghui>
2019-06-10 17:22:24 +0800

    1.81 trade record page

---
+++ lib/page/home/record/trade_record_page.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+
+class TradeRecordPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TradeRecordPageState();
+  }
+
+}
+
+class TradeRecordPageState extends State<TradeRecordPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text("123123123"),
+      ),
+    );
+  }
+}
